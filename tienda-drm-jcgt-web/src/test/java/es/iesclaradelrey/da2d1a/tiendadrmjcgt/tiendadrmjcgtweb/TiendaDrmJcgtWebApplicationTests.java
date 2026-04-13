ae4f57c662188bb00c5950e7e0f261c02eba7349
PortatilNuevo <portail@nuevo.com>
2026-04-13 13:06:56 +0200

    Modulo web creado

---
+++ tienda-drm-jcgt-web/src/test/java/es/iesclaradelrey/da2d1a/tiendadrmjcgt/tiendadrmjcgtweb/TiendaDrmJcgtWebApplicationTests.java
@@ -0,0 +1,13 @@
+package es.iesclaradelrey.da2d1a.tiendadrmjcgt.tiendadrmjcgtweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TiendaDrmJcgtWebApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
