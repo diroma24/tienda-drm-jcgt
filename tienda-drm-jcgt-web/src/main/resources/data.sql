f11bd63feedae7b7e2e93b2b699ed30d3f9f0498
PortatilNuevo <portail@nuevo.com>
2026-04-21 11:34:59 +0200

    DataInitializer eliminado, creación de datos mediante SQL

---
+++ tienda-drm-jcgt-web/src/main/resources/data.sql
@@ -0,0 +1,11 @@
+INSERT INTO categorias (nombre, descripcion, imagen) VALUES
+    ('Sobres', 'Ampliaciones de las últimas ediciones como Escarlata y Púrpura.', 'sobres.jpg');
+
+INSERT INTO categorias (nombre, descripcion, imagen) VALUES
+    ('Mazos', 'Mazos listos para jugar, ideales para entrenadores principiantes.', NULL);
+
+INSERT INTO categorias (nombre, descripcion, imagen) VALUES
+    ('Accesorios', 'Protege tus cartas con las mejores fundas y cajas de almacenamiento.', 'accesorios.jpg');
+
+INSERT INTO categorias (nombre, descripcion, imagen) VALUES
+    ('Cartas', 'Cartas raras, Full Art y Secretas vendidas por separado.', 'cartas.jpg');
