fa8cd9d98b9615a43087f761d138d25997df7345
PortatilNuevo <portail@nuevo.com>
2026-04-23 12:01:18 +0200

    Html editado

--- tienda-drm-jcgt-web/src/main/java/es/iesclaradelrey/da2d1a/tiendadrmjcgt/web/controllers/InicioController.java
+++ tienda-drm-jcgt-web/src/main/java/es/iesclaradelrey/da2d1a/tiendadrmjcgt/web/controllers/InicioController.java
@@ -1,6 +1,9 @@
 package es.iesclaradelrey.da2d1a.tiendadrmjcgt.web.controllers;
 
+import es.iesclaradelrey.da2d1a.tiendadrmjcgt.common.services.CategoriaService;
+import es.iesclaradelrey.da2d1a.tiendadrmjcgt.common.services.ProductoService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 /**
@@ -13,6 +16,14 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller // Indica que la clase actúa como controlador web y retorna vistas lógicas
 public class InicioController {
 
+    private final ProductoService productoService;
+    private final CategoriaService categoriaService;
+
+    public InicioController(ProductoService productoService, CategoriaService categoriaService) {
+        this.productoService = productoService;
+        this.categoriaService = categoriaService;
+    }
+
     /**
      * Procesa la petición de acceso a la URL raíz del sistema.
      * <p>
@@ -22,8 +33,10 @@ public class InicioController {
      * * @return Nombre de la vista "home" que será procesada por el motor de plantillas.
      */
     @GetMapping("/")
-    public String index() {
+    public String index(Model model) {
         // Retorna la vista dinámica home.html ubicada en la carpeta de plantillas (templates)
+        model.addAttribute("novedades", productoService.findAll());
+        model.addAttribute("categorias", categoriaService.findAll());
         return "home";
     }
 }
