b6fd1d36c0292b41c93a10205f7bf2584ec4dd88
PortatilNuevo <portail@nuevo.com>
2026-05-05 10:22:47 +0200

    Security funciona

--- tienda-drm-jcgt-web/src/main/java/es/iesclaradelrey/da2d1a/tiendadrmjcgt/web/WebApp.java
+++ tienda-drm-jcgt-web/src/main/java/es/iesclaradelrey/da2d1a/tiendadrmjcgt/web/WebApp.java
@@ -2,12 +2,13 @@ package es.iesclaradelrey.da2d1a.tiendadrmjcgt.web;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.persistence.autoconfigure.EntityScan;
+import org.springframework.boot.autoconfigure.domain.EntityScan; // Import corregido
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication(scanBasePackages = {
         "es.iesclaradelrey.da2d1a.tiendadrmjcgt.web",
-        "es.iesclaradelrey.da2d1a.tiendadrmjcgt.common"
+        "es.iesclaradelrey.da2d1a.tiendadrmjcgt.common",
+        "es.iesclaradelrey.da2d1a.tiendadrmjcgt.security"
 })
 @EnableJpaRepositories(basePackages = "es.iesclaradelrey.da2d1a.tiendadrmjcgt.common.repositories")
 @EntityScan(basePackages = "es.iesclaradelrey.da2d1a.tiendadrmjcgt.common.entities")
@@ -16,5 +17,4 @@ public class WebApp {
     public static void main(String[] args) {
         SpringApplication.run(WebApp.class, args);
     }
-
 }
